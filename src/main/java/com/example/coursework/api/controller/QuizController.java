889a473d2797d800d1ea7c1934eac8e36d111021
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-01-06 17:47:10 +0200

    add quiz service

--- src/main/java/com/example/coursework/api/controller/QuizController.java
+++ src/main/java/com/example/coursework/api/controller/QuizController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
+@RequestMapping("/quizzes")
 public class QuizController {
 
     private final QuizService service;
@@ -20,31 +21,31 @@ public class QuizController {
     }
 
     @Async
-    @GetMapping("/quizzes")
+    @GetMapping
     public CompletableFuture<List<Quiz>> getAll() {
         return CompletableFuture.completedFuture(service.getAllQuiz());
     }
 
     @Async
-    @GetMapping("/quizzes/{id}")
+    @GetMapping("/{id}")
     public CompletableFuture<Quiz> getById(@PathVariable Integer id) {
         return CompletableFuture.completedFuture(service.getQuizById(id));
     }
 
     @Async
-    @PostMapping("/quizzes")
+    @PostMapping
     public CompletableFuture<Quiz> newQuiz(@RequestBody Quiz newQuestion) {
         return CompletableFuture.completedFuture(service.createQuiz(newQuestion));
     }
 
     @Async
-    @PutMapping("/quizzes/{id}")
-    public CompletableFuture<Quiz> updateQuiz(@RequestBody Quiz newQuestion, @PathVariable Integer id) {
+    @PutMapping("/{id}")
+    public CompletableFuture<Quiz> replaceQuiz(@RequestBody Quiz newQuestion, @PathVariable Integer id) {
         return CompletableFuture.completedFuture(service.updateQuiz(id, newQuestion));
     }
 
     @Async
-    @DeleteMapping("/quizzes/{id}")
+    @DeleteMapping("/{id}")
     public CompletableFuture<Void> deleteQuiz(@PathVariable Integer id) {
         return CompletableFuture.completedFuture(service.deleteQuiz(id));
     }
--- src/test/java/com/example/coursework/QuizControllerTest.java
+++ src/test/java/com/example/coursework/QuizControllerTest.java
@@ -2,8 +2,8 @@ package com.example.coursework;
 
 import com.example.coursework.api.controller.QuizController;
 import com.example.coursework.api.model.Quiz;
+import com.example.coursework.api.service.QuizService;
 import com.example.coursework.exceptions.QuizNotFoundException;
-import com.example.coursework.repos.QuizRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,7 +12,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,7 +22,7 @@ import static org.mockito.Mockito.*;
 public class QuizControllerTest {
 
     @Mock
-    private QuizRepository mockRepository;
+    private QuizService mockService;
 
     @InjectMocks
     private QuizController quizController;
@@ -32,7 +31,7 @@ public class QuizControllerTest {
     public void testGetAllQuizzes() {
         List<Quiz> mockQuizzes = Arrays.asList(new Quiz(1, "Quiz1", "Description1"),
                 new Quiz(2, "Quiz2", "Description2"));
-        when(mockRepository.findAll()).thenReturn(mockQuizzes);
+        when(mockService.getAllQuiz()).thenReturn(mockQuizzes);
 
         List<Quiz> result = quizController.getAll().join();
 
@@ -42,7 +41,7 @@ public class QuizControllerTest {
     @Test
     public void testGetQuizById() {
         Quiz mockQuiz = new Quiz(1, "Quiz1", "Description1");
-        when(mockRepository.findById(1)).thenReturn(Optional.of(mockQuiz));
+        when(mockService.getQuizById(1)).thenReturn(mockQuiz);
 
         Quiz result = quizController.getById(1).join();
 
@@ -52,7 +51,7 @@ public class QuizControllerTest {
     @Test
     public void testPostQuiz() {
         Quiz newQuiz = new Quiz(null, "New Quiz", "New Description");
-        when(mockRepository.save(any(Quiz.class))).thenReturn(newQuiz);
+        when(mockService.createQuiz(any(Quiz.class))).thenReturn(newQuiz);
 
         Quiz result = quizController.newQuiz(newQuiz).join();
 
@@ -61,11 +60,9 @@ public class QuizControllerTest {
 
     @Test
     public void testPutQuiz() {
-        Quiz existingQuiz = new Quiz(1, "Existing Quiz", "Existing Description");
         Quiz newQuiz = new Quiz(1, "Updated Quiz", "Updated Description");
 
-        when(mockRepository.findById(1)).thenReturn(Optional.of(existingQuiz));
-        when(mockRepository.save(any(Quiz.class))).thenReturn(newQuiz);
+        when(mockService.updateQuiz(1, newQuiz)).thenReturn(newQuiz);
 
         Quiz result = quizController.replaceQuiz(newQuiz, 1).join();
 
@@ -74,17 +71,17 @@ public class QuizControllerTest {
 
     @Test
     public void testDeleteQuiz() {
-        when(mockRepository.existsById(1)).thenReturn(true);
+        doNothing().when(mockService).deleteQuiz(1);
 
         CompletableFuture<Void> result = quizController.deleteQuiz(1);
 
         assertNull(result.join());
-        verify(mockRepository, times(1)).deleteById(1);
+        verify(mockService, times(1)).deleteQuiz(1);
     }
 
     @Test(expected = QuizNotFoundException.class)
     public void testDeleteQuizNotFound() {
-        when(mockRepository.existsById(1)).thenReturn(false);
+        doThrow(new QuizNotFoundException(1)).when(mockService).deleteQuiz(1);
 
         CompletableFuture<Void> result = quizController.deleteQuiz(1);
 
